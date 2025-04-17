1bc07c22ff90bdb975d9dc10e7307ede51862e70
jago-1009 <garwoodj22@gmail.com>
2025-04-17 11:56:53 -0400

    Refactor authentication handler to use query parameters for token fetching

--- api/auth.js
+++ api/auth.js
@@ -1,24 +1,24 @@
 export default async function handler(req, res) {
   const { code } = req.body;
-//   localStorage.setItem("item", authData.access_token);
+  //   localStorage.setItem("item", authData.access_token);
   // Use the access token to fetch data from Basecamp API
 
   if (!code) {
     return res.status(418).json({ error: "Access token not found" });
   }
-  const response = await fetch(`https://launchpad.37signals.com/authorization/token` + 
-    "?type=web_server" +
-    "&client_id=7e1d387ff157828dcf06ba3ebf5abbf035c03d6b" +
-    "&client_secret=" + process.env.CLIENT_SECRET +
-    "&redirect_uri=https://outlook-api.vercel.app" +
-    "&type=verification-code" 
-    )
-  
+  const response = await fetch(
+    `https://launchpad.37signals.com/authorization/token` +
+      "?type=web_server" +
+      "&client_id=7e1d387ff157828dcf06ba3ebf5abbf035c03d6b" +
+      "&client_secret=" +
+      process.env.CLIENT_SECRET +
+      "&redirect_uri=https://outlook-api.vercel.app" +
+      "&type=verification-code",
+    {method: "POST"}
+  );
 
   if (!response.ok) {
-    return res
-      .status(response.status)
-      .json({ error: "Failed to fetch token" });
+    return res.status(response.status).json({ error: "Failed to fetch token" });
   }
 
   const data = await response.json();
