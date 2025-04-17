dae4156b7caa75b710080b06f6b79ef2de7654e4
jago-1009 <garwoodj22@gmail.com>
2025-04-17 11:33:49 -0400

    Refactor getInProgress function to use token from request body and update fetch call to include token in the request

--- api/getInProgress.js
+++ api/getInProgress.js
@@ -1,14 +1,14 @@
 
 export default async function handler(req, res) {
 
- const accessToken = localStorage.getItem('access_token');
+ const {token } = req.body;
 
-    if (!accessToken) {
+    if (!token) {
       return res.status(401).json({ error: 'Access token not found' });
     }  
     const response = await fetch(`https://3.basecamp.com/3537899/buckets/38519896/todolists/7661448591/todos.json`, {
       headers: {
-        'Authorization': `Bearer ${accessToken}`,
+        'Authorization': `Bearer ${token}`,
         'User-Agent': 'Localhost-Test (garwoodj22@gmail.com)'
       }
     });
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
     }
   
     const data = await response.json();
-    res.status(200).json(data);
+   return res.status(200).json(data);
   }
 
 //   https://launchpad.37signals.com/authorization/new?type=web_server&client_id=7e1d387ff157828dcf06ba3ebf5abbf035c03d6b&redirect_uri=https://outlook-api.vercel.app
