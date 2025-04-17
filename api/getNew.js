79455a1188afd7c45cef2b0c4130e2ba4bc7b9a3
jago-1009 <garwoodj22@gmail.com>
2025-04-17 13:03:48 -0400

    Add getCompleted and getNew API handlers for fetching completed and new Basecamp todos

---
+++ api/getNew.js
@@ -0,0 +1,30 @@
+
+export default async function handler(req, res) {
+
+    const { token, accountID } = req.body;
+    console.log('req.body:', req.body);
+   
+       if (!token) {
+         return res.status(418).json({ error: 'Access token not found' });
+       }  
+       const response = await fetch(`https://3.basecampapi.com/3537899/buckets/38519896/todolists/7663592669/todos.json`, {
+           // 38519896
+         headers: {
+           'Authorization': `Bearer ${token}`,
+           'User-Agent': 'Localhost-Test (garwoodj22@gmail.com)',
+           'Content-Type': 'application/json'
+         }
+       });
+     
+       if (!response.ok) {
+   
+         return res.status(response.status).json({ error: 'Failed to fetch Basecamp data' + response.statusText  });
+       }
+     
+       const data = await response.json();
+      return res.status(200).json(data);
+     }
+   
+   //   https://launchpad.37signals.com/authorization/new?type=web_server&client_id=7e1d387ff157828dcf06ba3ebf5abbf035c03d6b&redirect_uri=https://outlook-api.vercel.app
+   // https://3.basecamp.com/3537899/buckets/1/todolists/2/todos.json
+   //https://3.basecamp.com/3537899/buckets/38519896/todolists/7661448591/todos.json
