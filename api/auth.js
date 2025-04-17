826e640b4fa014384d40ca9cdee3ba3ea7de7279
jago-1009 <garwoodj22@gmail.com>
2025-04-17 11:55:05 -0400

    Refactor token fetching logic in authentication handler to use query parameters instead of POST request

--- api/auth.js
+++ api/auth.js
@@ -6,24 +6,19 @@ export default async function handler(req, res) {
   if (!code) {
     return res.status(418).json({ error: "Access token not found" });
   }
-  const response = await fetch(`https://launchpad.37signals.com/oauth/token`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: new URLSearchParams({
-      client_id: "7e1d387ff157828dcf06ba3ebf5abbf035c03d6b",
-      client_secret: process.env.CLIENT_SECRET,
-      redirect_uri: "https://outlook-api.vercel.app",
-      type: "authorization_code",
-        code: code,
-    }),
-  });
+  const response = await fetch(`https://launchpad.37signals.com/authorization/token` + 
+    "?type=web_server" +
+    "&client_id=7e1d387ff157828dcf06ba3ebf5abbf035c03d6b" +
+    "&client_secret=" + process.env.CLIENT_SECRET +
+    "&redirect_uri=https://outlook-api.vercel.app" +
+    "&type=verification-code" 
+    )
+  
 
   if (!response.ok) {
     return res
       .status(response.status)
-      .json({ error: "Failed to fetch Basecamp data" });
+      .json({ error: "Failed to fetch token" });
   }
 
   const data = await response.json();
